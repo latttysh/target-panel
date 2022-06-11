1eb12b46088b7c6bd7c6ee5bc52c9cf199321328
latttysh <semyon.latysh@gmail.com>
2022-06-11 09:48:48 +0300

    checkbox styles

--- src/pages/Promo/index.jsx
+++ src/pages/Promo/index.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import NavBar from '../../components/NavBar';
-import SubHeader from '../../components/SubHeader';
-import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
-import TextField from '@mui/material/TextField';
-import Table from '../../components/Table';
-import './promo.scss';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import NavBar from "../../components/NavBar";
+import SubHeader from "../../components/SubHeader";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import TextField from "@mui/material/TextField";
+import Table from "../../components/Table";
+import "./promo.scss";
+import { useForm } from "react-hook-form";
 
 function Promo() {
   const {
@@ -19,7 +19,7 @@ function Promo() {
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => console.log(data);
-  const [value, setValue] = React.useState('1');
+  const [value, setValue] = React.useState("1");
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
@@ -27,16 +27,36 @@ function Promo() {
   return (
     <div className="promo">
       <NavBar />
-      <SubHeader title={'Промоматериалы'} />
-      <Box sx={{ width: '100%', typography: 'body1' }}>
+      <SubHeader title={"Промоматериалы"} />
+      <Box sx={{ width: "100%", typography: "body1" }}>
         <TabContext value={value}>
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
             <TabList onChange={handleChange} aria-label="lab API tabs example">
-              <Tab style={{ color: 'white', marginLeft: '10px' }} label="Баннеры" value="1" />
-              <Tab style={{ color: 'white', marginRight: '20px' }} label="Формы" value="2" />
-              <Tab style={{ color: 'white', marginRight: '20px' }} label="Лендинги" value="3" />
-              <Tab style={{ color: 'white', marginRight: '20px' }} label="Метки" value="4" />
-              <Tab style={{ color: 'white', marginRight: '20px' }} label="Postback" value="5" />
+              <Tab
+                style={{ color: "white", marginLeft: "10px" }}
+                label="Баннеры"
+                value="1"
+              />
+              <Tab
+                style={{ color: "white", marginRight: "20px" }}
+                label="Формы"
+                value="2"
+              />
+              <Tab
+                style={{ color: "white", marginRight: "20px" }}
+                label="Лендинги"
+                value="3"
+              />
+              <Tab
+                style={{ color: "white", marginRight: "20px" }}
+                label="Метки"
+                value="4"
+              />
+              <Tab
+                style={{ color: "white", marginRight: "20px" }}
+                label="Postback"
+                value="5"
+              />
             </TabList>
           </Box>
           <TabPanel value="1">
@@ -55,35 +75,36 @@ function Promo() {
                   id="outlined-basic"
                   label="Введите название"
                   variant="outlined"
-                  style={{ width: '700px' }}
+                  style={{ width: "700px" }}
                 />
                 <button className="marcs-btn">Добавить</button>
               </div>
               <Table
                 className="table"
                 col={[
-                  { field: 'id', headerName: 'id', width: 120 },
-                  { field: 'title', headerName: 'Название', width: 350 },
+                  { field: "id", headerName: "id", width: 120 },
+                  { field: "title", headerName: "Название", width: 350 },
                   {
-                    field: 'url',
-                    headerName: 'Ссылка',
+                    field: "url",
+                    headerName: "Ссылка",
                     width: 750,
                   },
                 ]}
                 ro={[
                   {
-                    id: '1',
-                    title: 'Метка номер 1',
-                    url: 'тут будет очень длинная ссылка воооооот такая длинная',
+                    id: "1",
+                    title: "Метка номер 1",
+                    url: "тут будет очень длинная ссылка воооооот такая длинная",
                   },
                 ]}
               />
               <div className="text">
                 <div className="text-title">Метки для каналов</div>
                 <div className="text-descr">
-                  Рекомендуем использовать метки для разделения каналов трафика в статистике.
-                  Например, вы привлекаете трафик с помощью Директа и сетки сайтов. Добавьте метки:
-                  директ и сайты. В ссылке, идентификатор метки — ?partnerid=[id]
+                  Рекомендуем использовать метки для разделения каналов трафика
+                  в статистике. Например, вы привлекаете трафик с помощью
+                  Директа и сетки сайтов. Добавьте метки: директ и сайты. В
+                  ссылке, идентификатор метки — ?partnerid=[id]
                 </div>
               </div>
             </div>
@@ -94,31 +115,53 @@ function Promo() {
                 id="outlined-basic"
                 label="Вставьте ссылку, сгенерированную вашей TDS системой"
                 variant="outlined"
-                style={{ width: '700px' }}
+                style={{ width: "700px" }}
               />
-              <hr style={{ marginTop: '20px' }} />
+              <hr style={{ marginTop: "20px" }} />
               <div className="content">
                 <form onSubmit={handleSubmit(onSubmit)}>
                   <div className="request">
-                    <div className="request-title">Когда отправлять запрос?</div>
-                    <div className="request-checkbox">
-                      <input type="checkbox" placeholder="Option1" {...register('Option-1')} />
-                      <div className="request-checkbox-title">Опция 1</div>
+                    <div className="request-title">
+                      Когда отправлять запрос?
                     </div>
-                    <div className="request-checkbox">
-                      <input type="checkbox" placeholder="Option2" {...register('Option-2')} />
-                      <div className="request-checkbox-title">Опция 2</div>
+                    <div class="checkboxes__row">
+                      <div class="checkboxes__item">
+                        <label class="checkbox style-c">
+                          <input type="checkbox" />
+                          <div class="checkbox__checkmark"></div>
+                          <div class="checkbox__body">Опция 1 </div>
+                        </label>
+                      </div>
                     </div>
-                    <div className="request-checkbox">
-                      <input type="checkbox" placeholder="Option2" {...register('Option-3')} />
-                      <div className="request-checkbox-title">Опция 3</div>
+                    <div class="checkboxes__row">
+                      <div class="checkboxes__item">
+                        <label class="checkbox style-c">
+                          <input type="checkbox" />
+                          <div class="checkbox__checkmark"></div>
+                          <div class="checkbox__body">Опция 2</div>
+                        </label>
+                      </div>
                     </div>
-                    <div className="request-checkbox">
-                      <input type="checkbox" placeholder="Option2" {...register('Option-4')} />
-                      <div className="request-checkbox-title">Опция 4</div>
+                    <div class="checkboxes__row">
+                      <div class="checkboxes__item">
+                        <label class="checkbox style-c">
+                          <input type="checkbox" />
+                          <div class="checkbox__checkmark"></div>
+                          <div class="checkbox__body">Опция 3</div>
+                        </label>
+                      </div>
+                    </div>
+                    <div class="checkboxes__row">
+                      <div class="checkboxes__item">
+                        <label class="checkbox style-c">
+                          <input type="checkbox" />
+                          <div class="checkbox__checkmark"></div>
+                          <div class="checkbox__body">Опция 4</div>
+                        </label>
+                      </div>
                     </div>
                     <div className="request-title">Метод</div>
-                    <select {...register('method')}>
+                    <select {...register("method")}>
                       <option value="post">POST</option>
                       <option value="get">GET</option>
                     </select>
